43a015d0ab52218cc640984f8b3cd6cd0fdf1389
JorgeAlanRISOSUIT <JAlanEscobarFlores@outlook.com>
2024-06-03 11:46:53 -0600

    Se agregaron los SP y se cambiaron los modelos

--- DL/Partial.cs
+++ DL/Partial.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,57 @@ namespace DL
 {
     public partial class JAEscobarConsumoAJAXContext
     {
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder) { 
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder) {
         }
 
+        public List<DL.PersonaAseguradora> GetPersonaAseguradoras()
+        {
+            return PersonaAseguradoras.FromSqlRaw("MostrarPersonaAseguradora").ToList();
+        }
 
+        public PersonaAseguradora GetPersonaAseguradora(int idPersona)
+        {
+            return PersonaAseguradoras.FromSqlInterpolated($"MostrarPersonaAseguradoraById {idPersona}").SingleOrDefault();
+        }
+
+        public int AddPersonaAseguradora(ML.PersonaAseguradora aseguradora)
+        {
+            return Database.ExecuteSqlInterpolated($"AgregarPersonaAseguradora {aseguradora.Nombre}, {aseguradora.ApellidoPaterno}, {aseguradora.ApellidoMaterno}, {aseguradora.EstadoCivil}, {aseguradora.Genero}, {aseguradora.FechaNacimiento}, {aseguradora.Entidad.IdEntidad}, {aseguradora.CURP}, {aseguradora.RFC}, {aseguradora.Telefono}, {aseguradora.Correo}");
+        }
+
+        public int UpdatePersonaAseguradora(ML.PersonaAseguradora aseguradora)
+        {
+            return Database.ExecuteSqlInterpolated($"ActualizarPersonaAseguradora {aseguradora.IdPersona}, {aseguradora.Nombre}, {aseguradora.ApellidoPaterno}, {aseguradora.ApellidoMaterno}, {aseguradora.EstadoCivil}, {aseguradora.Genero}, {aseguradora.FechaNacimiento}, {aseguradora.Entidad.IdEntidad}, {aseguradora.CURP}, {aseguradora.RFC}, {aseguradora.Telefono}, {aseguradora.Correo}");
+        }
+
+        public int DeletePersonaAseguradora(int idPersona)
+        {
+            return Database.ExecuteSqlInterpolated($"EliminarPersonaAseguradora {idPersona}");
+        }
+
+        public List<DL.PersonaEvento> GetPersonaEventos()
+        {
+            return PersonaEventos.FromSqlRaw("MostrarPersonaEvento").ToList();
+        }
+
+        public DL.PersonaEvento GetPersonaEvento(int idPersona)
+        {
+            return PersonaEventos.FromSqlRaw($"MostrarPersonaAseguradoraById {idPersona}").SingleOrDefault();
+        }
+
+        public int AddPersonaEvento(ML.PersonaEvento evento)
+        {
+            return Database.ExecuteSqlInterpolated($"AgregarPersonaEventos {evento.Nombre}, {evento.Telefono}, {evento.Email}, {evento.Empresa}");
+        }
+
+        public int UpdatePersonaEvento(ML.PersonaEvento evento)
+        {
+            return Database.ExecuteSqlInterpolated($"ActualizarPersonaEvento {evento.IdPersona}, {evento.Nombre}, {evento.Telefono}, {evento.Email}, {evento.Empresa}");
+        }
+
+        public int DeletePersonaEvento(int idPersona)
+        {
+            return Database.ExecuteSqlInterpolated($"EliminarPersonaEvento {idPersona}");
+        }
     }
 }
